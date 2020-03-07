e9af1821cd08d7f963978a021eb10ab0b21ae052
Rafael-Batista-Dev <59888617+Rafael-Batista-Dev@users.noreply.github.com>
2020-03-07 09:02:21 -0300

    Validação implementada no input

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,3 +1,15 @@
 module.exports.iniciaChat = function(application, req, res) {
-  res.render("chat");
+  var dadosForm = req.body;
+  req.assert("apelido", "Nome ou Apelido é obrigratório").notEmpty();
+  req
+    .assert("apelido", "Nome ou apelido deve conter entre 3 à 15 caracteres")
+    .isLength(3, 15);
+  var erros = req.validationErrors();
+
+  if (erros) {
+    res.render("index", { validacao: erros });
+    return;
+  }
+
+  res.render("chat", { dadosForm: dadosForm });
 };
