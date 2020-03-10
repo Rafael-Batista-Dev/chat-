a945e1a072ffb6593bd1bfd68b4b7a4cfab2619c
Rafael-Batista-Dev <59888617+Rafael-Batista-Dev@users.noreply.github.com>
2020-03-10 08:01:01 -0300

    Corregido error undefined no controller chat.js

--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -2,7 +2,7 @@ module.exports.iniciaChat = function(application, req, res) {
   var dadosForm = req.body;
   req.assert("apelido", "Nome ou Apelido é obrigratório").notEmpty();
   req
-    .assert("apelido", "Nome ou apelido deve conter entre 3 à 15 caracteres")
+    .assert("apelido", "Nome ou Apelido deve conter entre 3 à 15 caracteres")
     .isLength(3, 15);
   var erros = req.validationErrors();
 
@@ -11,10 +11,9 @@ module.exports.iniciaChat = function(application, req, res) {
     return;
   }
 
-  application.get("oi").emit("msgUser", {
+  application.get("io").emit("msgUser", {
     apelido: dadosForm.apelido,
     mensagem: "Acabou de entrar no chat"
   });
-
   res.render("chat", { dadosForm: dadosForm });
 };
